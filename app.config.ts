a195210cfb240b1009df4c316804aa02885967eb
crspradlin <github@crspradlin.org>
2022-09-30 22:43:40 -0400

    Added global app config

---
+++ app.config.ts
@@ -0,0 +1,6 @@
+export default defineAppConfig({
+    title: 'AWS Cognito App UI',
+    api: {
+        url: 'https://backend.api.url'
+    }
+})
--- app.vue
+++ app.vue
@@ -1,8 +1,10 @@
 <script lang="ts" setup>
   import './assets/css/tailwind.css'
+  const config = useAppConfig();
 </script>
 
 <template>
+  <title>{{ config.title }}</title>
   <div class="flex">
     <NuxtPage />
   </div>
