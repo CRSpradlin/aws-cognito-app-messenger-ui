72fc0a468a14410b5674e3f2d4f957f9586065d9
crspradlin <github@crspradlin.org>
2022-10-01 21:55:46 -0400

    added composable user state

---
+++ composables/states.ts
@@ -0,0 +1,10 @@
+export const useUser = () => {
+    return useState('user', () => {
+        return {
+            token: '',
+            name: '',
+            profile: '',
+            confirmed: false
+        }
+    });
+}
