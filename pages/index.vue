bb64bbccab7f7aafeac4d4dcd735bd3d15552114
crspradlin <github@crspradlin.org>
2022-10-02 21:34:27 -0400

    added rough first pass implementation of user registration

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,102 @@
+<script setup>
+const config = useAppConfig();
+const user = useUser();
+
+const loadingState = ref(false);
+const registeredUser = ref(false);
+const confirmedUser = ref(false);
+const registeredProfile = ref("");
+const email = ref("");
+const username = ref("");
+const password = ref("");
+const confirmation = ref("");
+
+const confirmUser = async () => {
+    loadingState.value = true;
+    const { data } = await useFetch(config.api.url+'/user/confirm', {
+        method: 'POST', 
+        body: {
+            username,
+            confirmation
+        }
+    });
+    loadingState.value = false;
+
+    confirmedUser.value = true;
+
+    console.log(data);
+}
+
+const registerUser = async () => {
+    loadingState.value = true;
+    // TODO: Handle non-200 responses
+    const { data } = await useFetch(config.api.url+'/user/register', {
+        method: 'POST', 
+        body: {
+            username,
+            email,
+            password
+        }
+    });
+    loadingState.value = false;
+
+    user.name = username;
+    user.profile = data.value['UserSub'];
+    user.confirmed = data.value['UserConfirmed'];
+
+    registeredProfile.value = user.profile;
+    registeredUser.value = true;
+}
+</script>
+
+
 <template>
     <div class="text-stone-200 bg-gradient-to-r from-teal-600 to-blue-600 grid place-items-center w-screen h-screen">
         <div class="flex flex-col content-center bg-cyan-900 p-12 rounded-xl">
-            <span class="text-4xl">Register</span>
-            <div class="flex flex-col">
-                <div class="flex flex-col p-2">
-                    <label class="text-2xl" for="email">Email</label>
-                    <input class="text-2xl rounded" name="email" type="text" />
+            <div v-if="confirmedUser">
+                <span class="text-4xl p-2">User Confirmed</span>
+            </div>
+            <div v-else>
+                <div v-if="registeredUser">
+                    <!-- TODO: Show confirmation form -->
+                    <div class="flex flex-col">
+                        <span class="text-4xl pr-2 pl-2 pt-2">Confirm</span>
+                        <span class="text-xl pr-2 pl-2">A confirmation code was sent to: {{email}}</span>
+                        <span class="text-xl pr-2 pl-2 pb-2">Please enter the code below and click confirm</span>
+                        <div class="flex flex-col p-2">
+                            <label class="text-2xl" for="confirmation">Confirmation Code</label>
+                            <input class="text-stone-800 text-2xl rounded" v-model="confirmation"  name="confirmation" type="text" />
+                        </div>
+                        <div class="flex flex-col p-2">
+                            <div @click="confirmUser()" class="flex flex-col text-stone-100 text-2xl text-center p-3 bg-violet-500 rounded-xl hover:bg-violet-400 active:bg-violet-600">
+                                <img v-if="loadingState" class="h-8 w-8 m-auto" src="/assets/images/loading.gif" />
+                                <span v-else>Confirm</span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="flex flex-col p-2">
-                    <label class="text-2xl" for="password">Password</label>
-                    <input class="text-2xl rounded" name="password" type="password" />
-                </div>
-                <div class="flex flex-col p-2">
-                    <button class="text-2xl p-3 bg-violet-500 rounded-xl hover:bg-violet-400 active:bg-violet-600">Submit</button>
+                <div v-else>
+                    <div class="flex flex-col">
+                        <span class="text-4xl p-2">Register</span>
+                        <div class="flex flex-col p-2">
+                            <label class="text-2xl" for="email">Email</label>
+                            <input class="text-stone-800 text-2xl rounded" v-model="email"  name="email" type="text" />
+                        </div>
+                        <div class="flex flex-col p-2">
+                            <label class="text-2xl" for="username">Username</label>
+                            <input class="text-stone-800 text-2xl rounded" v-model="username"  name="username" type="text" />
+                        </div>
+                        <div class="flex flex-col p-2">
+                            <label class="text-2xl" for="password">Password</label>
+                            <input class="text-stone-800 text-2xl rounded" v-model="password" name="password" type="password" />
+                        </div>
+                        <div class="flex flex-col p-2">
+                            <div @click="registerUser()" class="flex flex-col text-stone-100 text-2xl text-center p-3 bg-violet-500 rounded-xl hover:bg-violet-400 active:bg-violet-600">
+                                <img v-if="loadingState" class="h-8 w-8 m-auto" src="/assets/images/loading.gif" />
+                                <span v-else>Submit</span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
