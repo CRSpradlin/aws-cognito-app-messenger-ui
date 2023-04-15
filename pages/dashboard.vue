a9b252549dade69ed93267355ae2fbd80168295a
crspradlin <github@crspradlin.org>
2023-04-15 11:49:31 -0400

    Fix middleware refrence errors during auth check

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -3,13 +3,21 @@
 const user = useUser();
 const dynamicAnimationClass = ref('animate-fade-in-down');
 
+const logout = () => {
+    clearUser();
+    navigateTo('/');
+};
+
 </script>
 
 <template>
   <div class="text-stone-200 bg-gradient-to-r from-teal-600 to-blue-600 grid place-items-center w-screen h-screen">
     <div :class="dynamicAnimationClass" class="flex flex-col content-center bg-cyan-900 p-12 rounded-xl">
       <div class="flex flex-col">
-        {{ user.value.profile }}
+        {{ user.profile }}
+      </div>
+      <div class="flex flex-col text-stone-100 text-2xl text-center p-3 bg-violet-500 rounded-xl hover:bg-violet-400 active:bg-violet-600" @click="logout()">
+        <span>Log Out</span>
       </div>
     </div>
   </div>
