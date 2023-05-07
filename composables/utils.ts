d63975dda894f487f42c9747f67fd6cf154636f7
crspradlin <github@crspradlin.org>
2023-05-06 22:59:18 -0400

    Added authNeeded param for restricted endpoints

--- composables/utils.ts
+++ composables/utils.ts
@@ -1,14 +1,22 @@
-export const cognitoFetch = async (path, body) => {
+export const cognitoFetch = async (path, body, authNeeded = false) => {
+    const user = useUser();
     const config = useAppConfig();
 
-    const { data, error } = await useFetch(config.api.url + path, {
+    const request = {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            Authorization: authNeeded ? user.value.token : null
         },
         body,
         initialCache: false // Global definition causes cached response returns
-    });
+    };
+
+    if (request.body === null) {
+        delete request.body;
+    }
+
+    const { data, error } = await useFetch(config.api.url + path, request);
 
     return {
         data,
