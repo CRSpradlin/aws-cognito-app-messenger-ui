e7e829d5ff40cfb0cf0bfd018c609a8a29481109
crspradlin <github@crspradlin.org>
2023-05-12 22:11:45 -0400

    Blocked out setMessageData

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,22 +1,44 @@
 <script setup>
-
-const user = useUser();
-const dynamicAnimationClass = ref('animate-fade-in-down');
+const conversationData = ref([]);
 
 const logout = () => {
     clearUser();
     navigateTo('/');
 };
 
+cognitoFetch('/conversation', null, true).then(({ data }) => {
+    conversationData.value = data.value.conversations;
+});
+
+// const getConversationData = async () => {
+//     // TODO: Add error handling
+//     const { data, error } = await cognitoFetch('/conversation', null, true);
+//     conversationData.value = data.value.conversations;
+// };
+
+const setMessageData = (conversationId) => {
+    cognitoFetch(`/conversation/${conversationId}/message`, null, true).then(({ data, error }) => {
+        console.log(data);
+    });
+};
+
 </script>
 
 <template>
-  <div class="text-stone-200 bg-gradient-to-r from-teal-600 to-blue-600 grid place-items-center w-screen h-screen">
-    <div :class="dynamicAnimationClass" class="flex flex-col content-center bg-cyan-900 p-12 rounded-xl">
-      <div class="flex flex-col">
-        {{ user.profile }}
+  <div class="flex flex-row text-stone-200 bg-gradient-to-r from-teal-600 to-blue-600 w-screen h-screen">
+    <div class="w-1/4 flex flex-col">
+      <div v-for="conversation of conversationData" :key="conversation.id" :on-click="setMessageData(conversation.id)" class="m-2 text-center p-3 bg-cyan-900 hover:bg-cyan-700 rounded-xl">
+        <div>
+          {{ conversation.name }}
+        </div>
       </div>
-      <div class="flex flex-col text-stone-100 text-2xl text-center p-3 bg-violet-500 rounded-xl hover:bg-violet-400 active:bg-violet-600" @click="logout()">
+    </div>
+    <div class="w-3/4 m-2 p-3 flex flex-col bg-cyan-900 p-12 rounded-xl">
+      <div class="flex flex-col">
+        Messages
+        <!-- Add messages from conversations -->
+      </div>
+      <div class="flex flex-col text-stone-100 text-2xl text-center bg-violet-500 rounded-xl hover:bg-violet-400 active:bg-violet-600" @click="logout()">
         <span>Log Out</span>
       </div>
     </div>
