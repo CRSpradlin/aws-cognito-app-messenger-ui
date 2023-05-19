03fb5b54def772d38a5040f55474b4331a708d8e
crspradlin <github@crspradlin.org>
2023-05-18 22:55:01 -0400

    Updated dashboard styling to look more functional

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,5 +1,7 @@
 <script setup>
 const conversationData = ref([]);
+const messageData = ref([]);
+const selectedConversation = ref('');
 
 const logout = () => {
     clearUser();
@@ -16,30 +18,37 @@ cognitoFetch('/conversation', null, true).then(({ data }) => {
 //     conversationData.value = data.value.conversations;
 // };
 
-const setMessageData = (conversationId) => {
-    cognitoFetch(`/conversation/${conversationId}/message`, null, true).then(({ data, error }) => {
-        console.log(data);
+const setConversationId = (conversationId) => {
+    selectedConversation.value = conversationId;
+    getMessageData();
+};
+
+const getMessageData = () => {
+    cognitoFetch(`/conversation/${selectedConversation.value}/message`, null, true).then(({ data, error }) => {
+        messageData.value = data.value;
     });
 };
 
 </script>
 
 <template>
-  <div class="flex flex-row text-stone-200 bg-gradient-to-r from-teal-600 to-blue-600 w-screen h-screen">
-    <div class="w-1/4 flex flex-col">
-      <div v-for="conversation of conversationData" :key="conversation.id" :on-click="setMessageData(conversation.id)" class="m-2 text-center p-3 bg-cyan-900 hover:bg-cyan-700 rounded-xl">
-        <div>
-          {{ conversation.name }}
+  <div class="flex flex-col p-5 text-stone-200 bg-gradient-to-r from-teal-600 to-blue-600 w-screen h-screen">
+    <div class="flex flex-col text-stone-100 w-fit m-4 p-2 text-2xl text-center bg-violet-500 rounded-xl hover:bg-violet-400 active:bg-violet-600" @click="logout()">
+      <span>Log Out</span>
+    </div>
+    <div class="flex flex-row p-2">
+      <div class="w-1/4 flex flex-col">
+        <div v-for="conversation of conversationData" :key="conversation.id" :on-click="setConversationId(conversation.id)" class="m-2 text-center p-3 bg-cyan-900 hover:bg-cyan-700 rounded-xl">
+          <div>
+            {{ conversation.name }}
+          </div>
         </div>
       </div>
-    </div>
-    <div class="w-3/4 m-2 p-3 flex flex-col bg-cyan-900 p-12 rounded-xl">
-      <div class="flex flex-col">
-        Messages
-        <!-- Add messages from conversations -->
-      </div>
-      <div class="flex flex-col text-stone-100 text-2xl text-center bg-violet-500 rounded-xl hover:bg-violet-400 active:bg-violet-600" @click="logout()">
-        <span>Log Out</span>
+      <div class="w-3/4 m-2 p-3 flex flex-col bg-cyan-900 p-12 rounded-xl">
+        <div class="flex flex-col">
+          Messages
+          <!-- Add messages from conversations -->
+        </div>
       </div>
     </div>
   </div>
