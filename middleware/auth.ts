ea68fb6dd64597d69f5fd6ecc5c721f28dbd5c44
crspradlin <github@crspradlin.org>
2023-11-23 11:23:55 -0500

    Updated auth middleware logic

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,9 +1,17 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-    if (to.fullPath !== '/dashboard' && isAuthenticated()) {
-        return navigateTo('/dashboard');
-    } else if (to.fullPath === '/dashboard') {
-        return navigateTo('/');
+    console.log(JSON.stringify(to.fullPath));
+    if (isAuthenticated()) {
+        if (to.fullPath === '/login' || to.fullPath === '/register')
+            return navigateTo('/dashboard');
     } else {
-        //
+        if (to.fullPath === '/dashboard')
+            return navigateTo('/');
     }
+    // if (to.fullPath !== '/dashboard' && isAuthenticated()) {
+    //     return navigateTo('/dashboard');
+    // } else if (to.fullPath === '/dashboard' && !isAuthenticated()) {
+    //     return navigateTo('/');
+    // } else {
+    //     //
+    // }
 });
