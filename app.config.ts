015bf3bb36b0007d840b7e25f8340abd767d4939
crspradlin <github@crspradlin.org>
2023-12-03 16:48:39 -0500

    Added socket infra integration for UI

--- app.config.ts
+++ app.config.ts
@@ -2,5 +2,8 @@ export default defineAppConfig({
     title: 'AWS Cognito App UI',
     api: {
         url: 'https://rest.aws.crspradlin.org'
+    },
+    socket: {
+        uri: 'wss://socket.aws.crspradlin.org'
     }
 });
